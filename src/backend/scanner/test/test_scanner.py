00cbcaeb2bf3ff09243aada74647147ae2a93b44
irfansofyana <irfansofyana0305@gmail.com>
2021-03-12 00:00:55 +0700

    Init test for lexical analysis

---
+++ src/backend/scanner/test/test_scanner.py
@@ -0,0 +1,21 @@
+import unittest
+
+# TODO: Import Scanner module here
+
+class TestScanner(unittest.TestCase):
+    def test_result_of_scanning(self):
+        """
+        Test the result of process scanning
+        """
+
+        # TODO: Get input files
+        """ 
+            For each input file:
+                scanner_result = Scan(input)
+                read expected output
+                Check if the results obtained = Expected Ouput
+        """
+    
+
+if __name__=="__main__":
+    unittest.main()
