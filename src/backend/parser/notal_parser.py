02024b5f97f2db537746b75665a0231269cc3322
irfansofyana <irfansofyana0305@gmail.com>
2021-03-31 23:16:01 +0700

    Fix token for real literal number

--- src/backend/parser/notal_parser.py
+++ src/backend/parser/notal_parser.py
@@ -1,22 +1,25 @@
 import ply.yacc as yacc
-from src.backend.scanner.notal_scanner import NotalScanner
+from src.backend.scanner.tmp_notal_scanner import TMPScanner
 
 class NotalParser(object):
-    tokens = NotalScanner.tokens
+    tokens = TMPScanner.tokens
+    start = 'expression'
 
     # grammar declaration here
     def p_relational_op(self, p):
-        """relational_op: S_EQUAL
+        """relational_op : S_EQUAL
                 | S_NOT_EQUAL
                 | S_LESS_THAN_EQUAL
-                | S_GREATER_THAN_REQUAL
+                | S_GREATER_THAN_EQUAL
                 | S_LESS_THAN
                 | S_GREATER_THAN
                 | S_ELEMENT_OF
+                | RW_EQ
+                | RW_NEQ
         """
 
     def p_multiplicative_op(self, p):
-        """multiplicative_op: S_TIMES
+        """multiplicative_op : S_TIMES
                             | S_DIVIDE
                             | RW_DIV
                             | RW_MOD
@@ -24,30 +27,61 @@ class NotalParser(object):
         """
 
     def p_additive_op(self, p):
-        """additive_op: S_PLUS
+        """additive_op : S_PLUS
                     | S_MINUS
                     | RW_OR
                     | RW_XOR
         """
 
     def p_unary_op(self, p):
-        """unary_op: S_PLUS
+        """unary_op : S_PLUS
             |   S_MINUS
             |   RW_NOT
         """
 
     def p_expression(self, p):
-        """expression: expression relational_op additive_expression
+        """expression : expression relational_op additive_expression
                     |   additive_expression
         """
 
-    # end of grammar declaration
+    def p_additive_expression(self, p):
+        """additive_expression : additive_expression additive_op multiplicative_expression
+                            |   multiplicative_expression
+        """
 
+    def p_multiplicative_expression(self, p):
+        """multiplicative_expression : multiplicative_expression multiplicative_op unary_expression
+                                    |   unary_expression
+        """
+
+    def p_unary_expression(self, p):
+        """unary_expression : unary_op unary_expression
+                        |   primary_expression
+        """
+
+    # STILL NOT COMPLETE
+    def p_primary_expression(self, p):
+        """primary_expression : variable
+                            | L_INTEGER_NUMBER
+                            | L_STRING
+                            | L_REAL_NUMBER
+                            | L_BOOLEAN_TRUE
+                            | L_BOOLEAN_FALSE
+                            | L_CHARACTER
+                            | S_LEFT_BRACKET expression S_RIGHT_BRACKET
+        """
+
+    # STILL NOT COMPLETE
+    def p_variable(self, p):
+        """variable : IDENTIFIER
+        """
+
+    # end of grammar declaration
     def p_error(self, p):
         print("Syntax error on input!")
 
     def __init__(self):
-        self.lexer = NotalScanner()
+        self.lexer = TMPScanner()
         self.parser = yacc.yacc(module=self)
 
     def parse(self, source):
