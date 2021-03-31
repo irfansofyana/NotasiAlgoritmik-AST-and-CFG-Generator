c5da69db7d69c5e29d29a5e29af8b04ce9016088
irfansofyana <irfansofyana0305@gmail.com>
2021-04-01 00:32:23 +0700

    Structurized Parser and fix it

--- src/backend/parser/notal_parser.py
+++ src/backend/parser/notal_parser.py
@@ -5,7 +5,32 @@ class NotalParser(object):
     tokens = TMPScanner.tokens
     start = 'expression'
 
-    # grammar declaration here
+    def p_identifier_list(self, p):
+        """identifier_list  : identifier_list S_COMMA IDENTIFIER
+                            | IDENTIFIER
+        """
+
+    # STILL NOT COMPLETE
+    def p_constant(self, p):
+        """constant : L_INTEGER_NUMBER
+                    | L_STRING
+                    | L_REAL_NUMBER
+                    | L_BOOLEAN_TRUE
+                    | L_BOOLEAN_FALSE
+                    | L_CHARACTER
+                    | L_NIL
+        """
+
+    def p_variable(self, p):
+        """variable : IDENTIFIER
+                    | variable S_DOT field_id
+        """
+
+    def p_expression(self, p):
+        """expression : expression relational_op additive_expression
+                    |   additive_expression
+        """
+
     def p_relational_op(self, p):
         """relational_op : S_EQUAL
                 | S_NOT_EQUAL
@@ -18,12 +43,9 @@ class NotalParser(object):
                 | RW_NEQ
         """
 
-    def p_multiplicative_op(self, p):
-        """multiplicative_op : S_TIMES
-                            | S_DIVIDE
-                            | RW_DIV
-                            | RW_MOD
-                            | RW_AND
+    def p_additive_expression(self, p):
+        """additive_expression : additive_expression additive_op multiplicative_expression
+                            |   multiplicative_expression
         """
 
     def p_additive_op(self, p):
@@ -33,25 +55,17 @@ class NotalParser(object):
                     | RW_XOR
         """
 
-    def p_unary_op(self, p):
-        """unary_op : S_PLUS
-            |   S_MINUS
-            |   RW_NOT
-        """
-
-    def p_expression(self, p):
-        """expression : expression relational_op additive_expression
-                    |   additive_expression
-        """
-
-    def p_additive_expression(self, p):
-        """additive_expression : additive_expression additive_op multiplicative_expression
-                            |   multiplicative_expression
-        """
-
     def p_multiplicative_expression(self, p):
         """multiplicative_expression : multiplicative_expression multiplicative_op unary_expression
                                     |   unary_expression
+        """
+
+    def p_multiplicative_op(self, p):
+        """multiplicative_op : S_TIMES
+                            | S_DIVIDE
+                            | RW_DIV
+                            | RW_MOD
+                            | RW_AND
         """
 
     def p_unary_expression(self, p):
@@ -59,24 +73,23 @@ class NotalParser(object):
                         |   primary_expression
         """
 
+    def p_unary_op(self, p):
+        """unary_op : S_PLUS
+            |   S_MINUS
+            |   RW_NOT
+        """
+
     # STILL NOT COMPLETE
     def p_primary_expression(self, p):
         """primary_expression : variable
-                            | L_INTEGER_NUMBER
-                            | L_STRING
-                            | L_REAL_NUMBER
-                            | L_BOOLEAN_TRUE
-                            | L_BOOLEAN_FALSE
-                            | L_CHARACTER
+                            | constant
                             | S_LEFT_BRACKET expression S_RIGHT_BRACKET
         """
 
-    # STILL NOT COMPLETE
-    def p_variable(self, p):
-        """variable : IDENTIFIER
+    def p_field_id(self, p):
+        """field_id : IDENTIFIER
         """
 
-    # end of grammar declaration
     def p_error(self, p):
         print("Syntax error on input!")
 
