f8e28fc85cdeb733ef2dcf527bb8462c7ed4a359
irfansofyana <irfansofyana0305@gmail.com>
2021-04-24 23:12:06 +0700

    Fix logic CFG

--- src/backend/cfg_generator/cfg.py
+++ src/backend/cfg_generator/cfg.py
@@ -4,7 +4,6 @@ class CFG:
         self.entry_block = entry_block
         self.exit_block = exit_block
 
-        self.connect_entry_and_exit_block()
 
     def get_entry_block(self):
         return self.entry_block
@@ -20,11 +19,6 @@ class CFG:
         for exit_block in self.exit_block:
             exit_block.add_adjacent(entry_block_next_cfg)
         self.exit_block = cfg.get_exit_block()
-
-    def connect_entry_and_exit_block(self):
-        for exit_block in self.exit_block:
-            if self.entry_block.get_label() != exit_block.get_label():
-                self.entry_block.add_adjacent(exit_block)
 
     def get_graph(self, num_node):
         is_visited = [False for i in range(0, num_node + 1)]
