8792de5d94853fce6896fb7648dcf40c54388316
irfansofyana <irfansofyana0305@gmail.com>
2021-05-20 10:45:11 +0700

    Feat: implement test_cfg

--- src/backend/tests/integration/test_integration.py
+++ src/backend/tests/integration/test_integration.py
@@ -2,6 +2,7 @@ import unittest
 import os
 import json
 from src.api.functions import *
+from src.api.visualize_cfg import convert_cfg_to_graphviz
 
 
 class Integration(unittest.TestCase):
@@ -12,14 +13,20 @@ class Integration(unittest.TestCase):
         return input_src
 
     @staticmethod
-    def read_expected_output(file_name):
+    def read_expected_ast(file_name):
         with open(file_name, encoding='utf-8') as f:
             expected_tokens = json.load(f)
         return expected_tokens
 
+    @staticmethod
+    def read_expected_cfg(file_name):
+        with open(file_name, encoding='utf-8') as f:
+            expected_cfg = f.read()
+        return expected_cfg
+
     def test_ast(self):
         input_folder_dir = "input"
-        output_folder_dir = "output"
+        output_folder_dir = "output-ast"
 
         input_files = os.listdir(input_folder_dir)
         for input_file_name in input_files:
@@ -28,12 +35,25 @@ class Integration(unittest.TestCase):
 
             # Read expected AST
             output_file_name = f'{output_folder_dir}/{re.sub(".in", ".json", input_file_name)}'
-            expected_result = self.read_expected_output(output_file_name)
+            expected_result = self.read_expected_ast(output_file_name)
 
             self.assertEqual(generated_ast, expected_result)
 
     def test_cfg(self):
-        self.assertEqual(True, True)
+        input_folder_dir = "input"
+        output_folder_dir = "output-cfg"
+
+        input_files = os.listdir(input_folder_dir)
+        for input_file_name in input_files:
+            # Read generated CFG
+            generated_cfg = get_cfg(f'{input_folder_dir}/{input_file_name}')
+            generated_cfg = str(convert_cfg_to_graphviz(generated_cfg))
+
+            # Read expected CFG
+            output_file_name = f'{output_folder_dir}/{re.sub(".in", ".out", input_file_name)}'
+            expected_result = self.read_expected_cfg(output_file_name)
+
+            self.assertEqual(generated_cfg, expected_result)
 
 
 if __name__ == '__main__':
