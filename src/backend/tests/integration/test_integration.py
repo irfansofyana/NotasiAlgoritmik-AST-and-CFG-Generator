4d8406128567e31b1e4372d468651f337723dc7d
irfansofyana <irfansofyana0305@gmail.com>
2021-05-20 08:44:53 +0700

    Init: integration test

---
+++ src/backend/tests/integration/test_integration.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class Integration(unittest.TestCase):
+    def test_ast(self):
+        self.assertEqual(True, True)
+
+    def test_cfg(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
