0037855e50e059e7f79475cd0457f2e9177eeae7
irfansofyana <irfansofyana0305@gmail.com>
2021-05-20 12:58:36 +0700

    feat: add convert_cfg_json_to_graphviz function

--- src/api/visualize_cfg.py
+++ src/api/visualize_cfg.py
@@ -17,9 +17,30 @@ def convert_cfg_to_graphviz(cfg):
     return graph
 
 
+def convert_cfg_json_to_graphviz(cfg_json):
+    def merge_statements(statements):
+        statements_str = ''
+        for statement in statements:
+            if statements_str == '':
+                statements_str += statement
+            else:
+                statements_str += f'\n{statement}'
+        return statements_str
+
+    graph = Digraph(comment="CFG result")
+    for node in cfg_json["nodes"]:
+        graph.node(str(node['label']), str(merge_statements(node['statements'])))
+    for edge in cfg_json["edges"]:
+        graph.edge(str(edge['start_node_label']), str(edge['end_node_label']))
+
+    return graph
+
+
 def traverse_cfg(cfg, graph):
     for node in cfg:
         graph.node(str(node.get_label()), node.get_info_str())
+
+    for node in cfg:
         for adj_node in cfg[node]:
             graph.edge(str(node.get_label()), str(adj_node.get_label()))
 
